579dcc93ed8130ae31ea9dd0bedba0515831c02d
rodume1 <95700436+rodume1@users.noreply.github.com>
2022-08-20 19:04:26 +0100

    Artist Page list

---
+++ src/components/ArtistsTable.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+// receives a date argument
+// and parses it to string
+// .slice(-2) extracts the last 2 elements of an array
+// in this case, we are using it to extract the last 2 characters
+// to present the leading zeros in datetime format
+const parseData = (date) => {
+    date = new Date(date);
+    let finalDate = ('0' + date.getDate()).slice(-2) + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + date.getFullYear();
+    let finalTime = ('0' + date.getHours()).slice(-2) + ":" + ('0' + date.getMinutes()).slice(-2) + ":" + ('0' + date.getSeconds()).slice(-2);
+    return finalDate + ' ' + finalTime;
+}
+
+function THead() {
+  return (
+    <thead>
+      <tr>
+        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+          Nome
+        </th>
+        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+          Email
+        </th>
+        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+          Data de inscrição
+        </th>
+      </tr>
+    </thead>
+  );
+}
+
+const TBody = (props) => {
+  const rows = props.dataIN.map(artist => {
+    return (
+      <tr key={artist.id}>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">
+          <div>
+            <p className="text-gray-900 whitespace-no-wrap">
+              {artist.name}
+            </p>            
+          </div>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">
+            <div>
+              <p className="text-gray-900 whitespace-no-wrap">
+                {artist.email}
+              </p>
+            </div>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">
+            <div>
+              <p className="text-gray-900 whitespace-no-wrap">
+                {parseData(artist.registrationDate)}
+              </p>
+            </div>
+        </td>            
+      </tr>
+    );
+  });
+
+  return <tbody>{rows}</tbody>;
+};
+
+class ArtistsTable extends React.Component {
+  render() {
+    const { artistsDataIN } = this.props;
+
+    return (
+      // code of component : https://tailwindcomponents.com/components/tables
+      // *adjusted to fit our needs
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div>
+            <h2 className="text-2xl font-semibold leading-tight">Artistas</h2>
+          </div>
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
+                <THead />
+                <TBody dataIN={artistsDataIN}/>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ArtistsTable;
--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -1,15 +1,38 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ArtistsTable from "../components/ArtistsTable";
 
 // Artists component
 class Artists extends React.Component {
 
+  state = {
+    artists: []
+  }
+
+  componentDidMount() {
+    this.getArtists();
+  }
+
+  async getArtists() {
+    let data = await fetch("api/artistsAPI/")
+      .then(res => {
+        return res.json();
+      })
+      .catch(err => {
+        console.log(`Could not request musics from API. Error ${err}`);
+      });
+
+    this.setState({ artists: data });
+  }
+
   render() {    
+    const { artists } = this.state;
+
     return (
       <>
         <Navbar />
-        <div>artistas</div>
+        <ArtistsTable artistsDataIN={artists} />
         <Footer />
       </>
     );
