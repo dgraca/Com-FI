ada31a90573d13238d7c6fbd5b6ecca3216154bf
rodume1 <95700436+rodume1@users.noreply.github.com>
2022-08-20 18:19:45 +0100

    Genres Page list

---
+++ src/components/GenresTable.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+function THead() {
+  return (
+    <thead>
+      <tr>
+        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+          Título
+        </th>
+      </tr>
+    </thead>
+  );
+}
+
+const TBody = (props) => {
+  const rows = props.dataIN.map(genre => {
+    return (
+      <tr key={genre.id}>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">
+          <div>
+            <p className="text-gray-900 whitespace-no-wrap">
+              {genre.title}
+            </p>
+          </div>
+        </td>
+      </tr>
+    );
+  });
+
+  return <tbody>{rows}</tbody>;
+};
+
+class GenresTable extends React.Component {
+  render() {
+    const { genresDataIN } = this.props;
+
+    return (
+      // code of component : https://tailwindcomponents.com/components/tables
+      // *adjusted to fit our needs
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div>
+            <h2 className="text-2xl font-semibold leading-tight">Géneros</h2>
+          </div>
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
+                <THead />
+                <TBody dataIN={genresDataIN}/>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GenresTable;
--- src/pages/Genres.js
+++ src/pages/Genres.js
@@ -1,15 +1,37 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import GenresTable from "../components/GenresTable";
 
 // Genres component
 class Genres extends React.Component {
+  state = {
+    genres: []
+  }
+
+  componentDidMount() {
+    this.getGenres();
+  }
+
+  async getGenres() {
+    let data = await fetch("api/genresAPI/")
+      .then(res => {
+        return res.json();
+      })
+      .catch(err => {
+        console.log(`Could not request musics from API. Error ${err}`);
+      });
+
+    this.setState({ genres: data });
+  }
 
   render() {    
+    const { genres } = this.state;
+
     return (
       <>
         <Navbar />
-        <div>géneros</div>
+        <GenresTable genresDataIN={genres} />
         <Footer />
       </>
     );
