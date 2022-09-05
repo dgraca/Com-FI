4f0b5e71a5e5c2bd1b0bed844e323a9f43b008b6
danielgraca <danielgrascas@gmail.com>
2022-09-05 15:38:06 +0100

    env variable fix

--- src/pages/musics/Create.js
+++ src/pages/musics/Create.js
@@ -40,7 +40,7 @@ class MusicsCreate extends React.Component {
   // request the list of genres from API
   // and sets that list to the state's genres array
   async getGenres() {
-    let data = await fetch(`{process.env.REACT_APP_WEB_API}/genresAPI/`)
+    let data = await fetch(`${process.env.REACT_APP_WEB_API}/genresAPI/`)
       .then(res => {
         if (res.ok) {
           return res.json();
